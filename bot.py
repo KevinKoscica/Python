94e9e04a50e63bf1752ec65a14ef35f4d38f05d0
kjk707 <kevinkoscica@gmail.com>
2016-02-18 18:07:03 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@
 #this is a work in progress
 import time
 import praw
-
+#Change user to your user name.
 user_agent="user"
 
 r=praw.Reddit(user_agent=user_agent)
@@ -15,6 +15,7 @@ sub=r.get_subreddit('subreddit')
 for submission in sub.get_hot(limit=5):
     text=submission.selftext.lower()
     msg='[new submission](%s)' % submission.short_link
+#send a message with the latest submissions from the reddit of your choice. Again change user to your username.
 r.send_message('user', 'test', msg)
 already_Recieved.append(submission.id)
 print("message sent")
